5043e88c9b44c91b5db2124bb7c97420d41ee0bf
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-09-26 19:21:22 +0700

    CRUD: Update 2

--- src/main/java/org/example/java5fa25sd20206/service/StudentService.java
+++ src/main/java/org/example/java5fa25sd20206/service/StudentService.java
@@ -32,4 +32,14 @@ public class StudentService {
 
         studentRepository.deleteStudentById(id);
     }
+
+    public Student getStudentById(long id) {
+
+        return studentRepository.getStudentById(id);
+    }
+
+    public void updateStudent(Student student) {
+
+        studentRepository.updateStudent(student);
+    }
 }
