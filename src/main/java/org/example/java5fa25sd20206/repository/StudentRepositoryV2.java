9a84b158a4306f087215f750ef2daf02916b17f2
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-09-29 20:00:53 +0700

    CRUD: using JpaRepository interface

---
+++ src/main/java/org/example/java5fa25sd20206/repository/StudentRepositoryV2.java
@@ -0,0 +1,9 @@
+package org.example.java5fa25sd20206.repository;
+
+import org.example.java5fa25sd20206.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepositoryV2 extends JpaRepository<Student, Long> {
+
+
+}
--- src/main/java/org/example/java5fa25sd20206/service/StudentService.java
+++ src/main/java/org/example/java5fa25sd20206/service/StudentService.java
@@ -3,6 +3,7 @@ package org.example.java5fa25sd20206.service;
 import lombok.RequiredArgsConstructor;
 import org.example.java5fa25sd20206.entity.Student;
 import org.example.java5fa25sd20206.repository.StudentRepository;
+import org.example.java5fa25sd20206.repository.StudentRepositoryV2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.List;
 //@RequiredArgsConstructor
 public class StudentService {
 
+    /*
     private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
@@ -41,5 +43,38 @@ public class StudentService {
     public void updateStudent(Student student) {
 
         studentRepository.updateStudent(student);
+    }
+    */
+
+    final
+    StudentRepositoryV2 studentRepositoryV2;
+
+    public StudentService(StudentRepositoryV2 studentRepositoryV2) {
+        this.studentRepositoryV2 = studentRepositoryV2;
+    }
+
+    public List<Student> getAllStudents() {
+
+        return studentRepositoryV2.findAll();
+    }
+
+    public void saveStudent(Student student) {
+
+        this.studentRepositoryV2.save(student);
+    }
+
+    public void deleteStudentById(long id) {
+
+        studentRepositoryV2.deleteById(id);
+    }
+
+    public Student getStudentById(long id) {
+
+        return studentRepositoryV2.findById(id).get();
+    }
+
+    public void updateStudent(Student student) {
+
+        studentRepositoryV2.save(student);
     }
 }
