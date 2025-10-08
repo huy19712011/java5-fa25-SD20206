afbf3d568c036f80077fd4c53236ef775778d186
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-10-08 19:36:58 +0700

    CRUD w 2 tables: Delete, update

--- src/main/java/org/example/java5fa25sd20206/controller/ProductController.java
+++ src/main/java/org/example/java5fa25sd20206/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -55,6 +56,42 @@ public class ProductController {
         }
 
         productService.saveProduct(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/deleteProduct/{id}")
+    public String deleteProduct(@PathVariable("id") long id) {
+
+        productService.deleteProductById(id);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable("id") long id, Model model) {
+
+        // get product from db
+        Product product = productService.getProductById(id);
+
+        // send data
+        model.addAttribute("product", product);
+        model.addAttribute("categories", categoryService.getAllCategories());
+
+        return "views/update_product";
+
+    }
+
+    @PostMapping("/products/updateProduct")
+    public String updateProduct(@Valid @ModelAttribute("product") Product product,
+                                BindingResult bindingResult, Model model) {
+
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "views/update_product";
+        }
+
+        productService.updateProduct(product);
 
         return "redirect:/products";
     }
